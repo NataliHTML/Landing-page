d2f973156d47b68f76cefcb38843490657c7a416
NataliHTML <nataly07081992@gmail.com>
2021-12-23 21:47:26 +0300

    added site

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,20 @@
-$(function () {
-
-
+$(document).ready(function () {
+	$(".btn-ricardo").click(function () {
+		$(".btn-ricardo").hide();
+		$(".social-ricardo").fadeIn();
+	});
+	$(".btn-alberto").click(function () {
+		$(".btn-alberto").hide();
+		$(".social-alberto").fadeIn();
+	});
+	$(".btn-thomas").click(function () {
+		$(".btn-thomas").hide();
+		$(".social-thomas").fadeIn();
+	});
+	$('.btn-menu').click(function () {
+		$('.modal').show()
+	});
+	$('.btn-close').click(function () {
+		$('.modal').hide()
+	});
 });
